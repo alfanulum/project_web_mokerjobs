fd14e23c73782d161127cd46a13d01c44500e7dd
frakenzabb <azriel.dirga.100@gmail.com>
2025-05-21 04:16:35 +0700

    Rapikan data

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,15 @@ Route::get('/', [JobController::class, 'overview'])->name('overview');
 
 Route::get('/find_job', [JobController::class, 'findJob'])->name('find_job');
 
+//form 1
 Route::get('/post-job/step1', [JobController::class, 'formPostJobStep1'])->name('form_postjob_step1');
 Route::post('/post-job/step1', [JobController::class, 'storeStep1'])->name('store_step1');
+//form 2
 Route::get('/post-job/step2', [JobController::class, 'formPostJobStep2'])->name('form_postjob_step2');
 Route::post('/post-job/step2', [JobController::class, 'storeStep2'])->name('store_step2');
+//form 3
+Route::get('/post-job/step3', [JobController::class, 'formPostJobStep3'])->name('form_postjob_step3');
+Route::post('/post-job/step3', [JobController::class, 'storeStep3'])->name('store_step3');
 
 Route::get('/apply/{id}', [JobController::class, 'apply'])->name('apply.job');
 
@@ -21,20 +26,12 @@ Route::get('/post_job', function () {
 })->name('post_job');
 
 Route::get('/sign_in', function () {
-    return view('auth.sign_in'); // path: resources/views/auth/login.blade.php
+    return view('auth.sign_in'); 
 })->name('sign_in');
-
-Route::get('/sign_up', function () {
-    return view('auth.sign_up');
-})->name('sign_up');
 
 Route::get('admin', function () {
     return view('admin');
 })->name('admin');
-
-//form3
-Route::get('/post-job/step3', [JobController::class, 'formPostJobStep3'])->name('form_postjob_step3');
-Route::post('/post-job/step3', [JobController::class, 'storeStep3'])->name('store_step3');
 
 use App\Http\Controllers\FeedbackController;
 
